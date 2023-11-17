360177b764e1844183266891fe267a612d8a420c
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-17 18:36:19 +0100

    Add project files.

---
+++ Ch06Ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ch06Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Console.ReadKey();
+        }
+        static void Write()
+        {
+            Console.WriteLine("Text output from function.");
+        }
+    }
+}
