555c5a3b3c8c977447a4b0caf1d405e6e0dd91a2
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-20 19:05:11 +0100

    Variable scope exercises

--- Ch06Ex01/Program.cs
+++ Ch06Ex01/Program.cs
@@ -2,14 +2,23 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Write();
-            Console.ReadKey();
-        }
+        static string myString;
         static void Write()
         {
-            Console.WriteLine("Text output from function.");
+            string myString = "String defined in Write()";
+            Console.WriteLine("Now in Write()");
+            Console.WriteLine($"Local myString = {myString}");
+            Console.WriteLine($"Global myString = {Program.myString}");
+        }
+        static void Main(string[] args)
+        {
+            string myString = "String defined in Main()";
+            Program.myString = "Global string";
+            Write();
+            Console.WriteLine("\nNow in Main()");
+            Console.WriteLine($"Local myString = {myString}");
+            Console.WriteLine($"Global myString = {Program.myString}");
+            Console.ReadKey();
         }
     }
 }
